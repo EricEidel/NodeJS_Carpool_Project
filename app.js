d218b737862743f857ee0fcedc45b79a8317a43a
eidelber <eidelber@ca.ibm.com>
2014-10-30 16:30:27 -0400

    Some stuff changed.

--- app.js
+++ app.js
@@ -11,46 +11,10 @@ var express = require('express');   // The ExpressJS framework
 var cookieParser = require('cookie-parser');
 var express_session = require('express-session'); 
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var LDAPStrategy = require('./lib/passport-ldap');
-var permissions = require('./lib/permissions');
-
 var routes = require('./routes');
-var user = require('./routes/user');
+var index = require('./routes/index');
 
-/**
- * Setting up the authontication
-**/
-passport.use(new LDAPStrategy({
-    server: {
-      url: 'ldaps://bluepages.ibm.com:636'
-    },
-    base: 'o=ibm.com,ou=bluepages',
-    search: {
-      filter: 'ou=bluepages,o=ibm.com',
-     },
-    debug:true
-  },
-  function(person, done) {
-    return done(null, {
-        username:person.mail,
-        provider:"ldap",
-        id: person.serialNumber || person.uid,
-        notesId : person.notesId,
-        cn: person.cn,
-        displayName: person.callupName
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-      done(null,user);
-});
-
+var ibmdb = require('ibm_db');
 /**
  * Setup the Express engine
 **/
@@ -59,69 +23,60 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 
+/* Get the database stuff */
+var serviceName = 'SQLDB';
 
-/* comment */
+/*
+ * Recursively iterate through the object looking for the key, 
+ * return the index
+ */
+function findKey(obj,lookup) 
+{
+   for (var i in obj) 
+   {
+      if (typeof(obj[i])==="object") 
+      {
+         if (i.toUpperCase().indexOf(lookup) > -1) 
+         {
+            // Found the key
+            return i;
+         }
+         findKey(obj[i],lookup);
+      }
+   }
+   return -1;
+}
+
+var env = null;
+var key = -1;
+
+// Look for an entry in the VCAP_SERVICES environment variable that has 
+// the serviceName string in it
+if (process.env.VCAP_SERVICES) 
+{
+   env = JSON.parse(process.env.VCAP_SERVICES);
+   key = findKey(env,serviceName);
+}
+var credentials = null;
+if (env != null)
+	 credentials = env[key][0].credentials;
+
+var dsnString = null;
+
+if (credentials != null)	
+	dsnString = "DRIVER={DB2};DATABASE=" + credentials.db + ";UID=" + credentials.username + ";PWD=" + credentials.password + ";HOSTNAME=" + credentials.hostname + ";port=" + credentials.port;     
 
 // App.use
 app.use(cookieParser()); // Needed for the session part to work
 app.use(express_session({secret: 'OMG_bigSecretL33tz0rs', resave: true,	saveUninitialized: true})); // Define a session framework with a "secret"
 app.use(express.static(path.join(__dirname, 'views'))); // Makes all the content in "public" accessible
-app.use( bodyParser.json() );       // to support JSON-encoded bodies ({"name":"foo","color":"red"} <- JSON encoding)
-app.use( bodyParser.urlencoded({ extended: true }) ); // to support URL-encoded bodies (name=foo&color=red <- URL encoding)
-
-app.use(passport.initialize());
-app.use(passport.session());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies ( {"name":"foo","color":"red"} <- JSON encoding )
+app.use( bodyParser.urlencoded({ extended: true }) ); // to support URL-encoded bodies ( name=foo&color=red <- URL encoding )
 
 
-// Authentication
-
-// If user is not logged in, go to /, otherwise do the function.
-app.get('/', permissions.isUserNotLoggedIn('/test'), function(req, res)
-{
-	// Only happens if user is not logged in
-	res.redirect('/');
-});
-
-app.post('/',
-  passport.authenticate('ldap', {
-    successReturnToOrRedirect: '/test',
-    failureRedirect: '/?reason=failed'
-  })
-);
-
-app.get('/logout', function(req, res){
-  req.logout();
-  delete req.session.user;
-  
-  res.redirect('/');
-});
-
-
-// Sessions examples
-app.get('/set', function(req, res)
-{
-	req.session.userName = req.user.userName;
-	res.redirect('/test');
-});
-
-app.get('/unset', function(req, res)
-{
-	delete req.session.userName;
-	res.redirect('/test');
-});
-
-app.get('/test', function(req, res)
-{
-	if (req.session.user)
-	{
-		res.write(req.session.user.username);
-		res.write(req.session.user.cn[0]);
-	}
-	else
-		res.write("No user logged in");
-
-	res.end();
-});
+// Load home page
+app.get('/', index.get_form);
+app.post('/', index.post_form);
 
 
 /**
